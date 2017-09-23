d42a2a27a07382946402276205ccc6299c3da94f
Rochelle Valdez <rochellevee@gmail.com>
2017-09-23 13:22:16 -0700

    Updated isIsogram

--- isIsogram.js
+++ isIsogram.js
@@ -1,9 +1,9 @@
 //CODEWARS: is Isogram
-const isIsogram = (str) => {
-  let lowerCase = str.toLowerCase;
+function isIsogram = (str) => {
+  let lowerCase = str.toLowerCase();
   let result = true;
-  for (var i = 0; i < str.length; i++) {
-    if ((str.split(str[i]).length - 1) > 1) {
+  for (var i = 0; i < lowerCase.length; i++) {
+    if ((lowerCase.split(lowerCase[i]).length - 1) > 1) {
       result = false;
     }
   }
